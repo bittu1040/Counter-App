463cb30704d3debd97a1db58d7ef0d6ba81517dd
bittu <bittu.kumar@yulu.bike>
2021-03-01 21:06:20 +0530

    counter-app

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title: string = "Counter App";
+
+  count: number = 0;
+
+  handleIncrease = () => {
+    this.count = this.count + 1;
+  };
+
+  handleDecrease = () => {
+
+    if(this.count>0){
+
+    this.count = this.count - 1;
+    }
+
+    else{
+      alert("can not decrease less than 0")
+    }
+  };
+
+  handleReset = () => {
+    this.count = 0;
+  };
+}
